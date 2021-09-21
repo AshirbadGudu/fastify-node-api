e21ca389bcc5509a3578e3824df35eecea46cf86
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-09-21 21:50:59 +0530

    basic server in fastify

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-console.log("Running Server");
+const fastify = require("fastify")({
+  logger: true,
+});
+
+const PORT = 5000;
+
+fastify.get("/", (req, res) => {
+  res.send({
+    data: "fastify API",
+  });
+});
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT);
+  } catch (error) {
+    fastify.log(error);
+    process.exit(1);
+  }
+};
+
+start();
