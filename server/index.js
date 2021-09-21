3176a1af1c3944af41da64a9e21e312e0cedc8ea
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-09-21 21:58:55 +0530

    params in fastify

--- server/index.js
+++ server/index.js
@@ -4,11 +4,19 @@ const fastify = require("fastify")({
   logger: true,
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-fastify.get("/", (req, res) => {
+fastify.get("/products", (req, res) => {
   res.send({
     data: products,
+  });
+});
+
+fastify.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const product = products.find((item) => item.id == id);
+  res.send({
+    data: product,
   });
 });
 
